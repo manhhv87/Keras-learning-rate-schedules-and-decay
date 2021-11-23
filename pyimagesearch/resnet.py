c223282a464f0f1b7b7ad1925a1dd32d9c9c0ed1
manhhv87 <manhhv87@gmail.com>
2021-11-23 09:57:17 +0700

    re-format code

--- pyimagesearch/resnet.py
+++ pyimagesearch/resnet.py
@@ -13,97 +13,101 @@ from keras.layers import add
 from keras.regularizers import l2
 from keras import backend as K
 
+
 class ResNet:
-	@staticmethod
-	def residual_module(data, K, stride, chanDim, red=False,
-		reg=0.0001, bnEps=2e-5, bnMom=0.9):
-		# the shortcut branch of the ResNet module should be initialized as the input data
-		shortcut = data
+    @staticmethod
+    def residual_module(data, K, stride, chanDim, red=False, reg=0.0001, bnEps=2e-5, bnMom=0.9):
+        # the shortcut branch of the ResNet module should be
+        # initialize as the input (identity) data
+        shortcut = data
 
-		# first block of the ResNet module are the 1x1 CONVs
-		bn1 = BatchNormalization(axis=chanDim, epsilon=bnEps,
-			momentum=bnMom)(data)
-		act1 = Activation("relu")(bn1)
-		conv1 = Conv2D(int(K * 0.25), (1, 1), use_bias=False,
-			kernel_regularizer=l2(reg))(act1)
+        # the first block of the ResNet module are the 1x1 CONVs
+        bn1 = BatchNormalization(axis=chanDim, epsilon=bnEps,
+                                 momentum=bnMom)(data)
+        act1 = Activation("relu")(bn1)
+        conv1 = Conv2D(int(K * 0.25), (1, 1), use_bias=False,
+                       kernel_regularizer=l2(reg))(act1)
 
-		# second block of the ResNet module are the 3x3 CONVs
-		bn2 = BatchNormalization(axis=chanDim, epsilon=bnEps,
-			momentum=bnMom)(conv1)
-		act2 = Activation("relu")(bn2)
-		conv2 = Conv2D(int(K * 0.25), (3, 3), strides=stride,
-			padding="same", use_bias=False,
-			kernel_regularizer=l2(reg))(act2)
+        # the second block of the ResNet module are the 3x3 CONVs
+        bn2 = BatchNormalization(axis=chanDim, epsilon=bnEps,
+                                 momentum=bnMom)(conv1)
+        act2 = Activation("relu")(bn2)
+        conv2 = Conv2D(int(K * 0.25), (3, 3), strides=stride,
+                       padding="same", use_bias=False,
+                       kernel_regularizer=l2(reg))(act2)
 
-		# third block of the ResNet module is another set of 1x1
-		# CONVs
-		bn3 = BatchNormalization(axis=chanDim, epsilon=bnEps,
-			momentum=bnMom)(conv2)
-		act3 = Activation("relu")(bn3)
-		conv3 = Conv2D(K, (1, 1), use_bias=False,
-			kernel_regularizer=l2(reg))(act3)
+        # the third block of the ResNet module is another set of 1x1
+        # CONVs
+        bn3 = BatchNormalization(axis=chanDim, epsilon=bnEps,
+                                 momentum=bnMom)(conv2)
+        act3 = Activation("relu")(bn3)
+        conv3 = Conv2D(K, (1, 1), use_bias=False,
+                       kernel_regularizer=l2(reg))(act3)
 
-		# if we are to reduce the spatial size, apply a CONV layer to the shortcut
-		if red:
-			shortcut = Conv2D(K, (1, 1), strides=stride,
-				use_bias=False, kernel_regularizer=l2(reg))(act1)
+        # if we are to reduce the spatial size, apply a CONV layer to
+        # the shortcut
+        if red:
+            shortcut = Conv2D(K, (1, 1), strides=stride,
+                              use_bias=False, kernel_regularizer=l2(reg))(act1)
 
-		# adding together the shortcut and the final CONV
-		x = add([conv3, shortcut])
+        # add together the shortcut and the final CONV
+        x = add([conv3, shortcut])
 
-		# returning the addition as the output of the ResNet module
-		return x
+        # return the addition as the output of the ResNet module
+        return x
 
-	@staticmethod
-	def build(width, height, depth, classes, stages, filters,
-		reg=0.0001, bnEps=2e-5, bnMom=0.9, dataset="cifar"):
-		# initializing the input shape to be "channels last" and the
-		# channels dimension itself
-		inputShape = (height, width, depth)
-		chanDim = -1
+    @staticmethod
+    def build(width, height, depth, classes, stages, filters,
+              reg=0.0001, bnEps=2e-5, bnMom=0.9, dataset="cifar"):
+        # initialize the input shape to be "channels last" and the
+        # channels dimension itself
+        inputShape = (height, width, depth)
+        chanDim = -1
 
-		# if we are using "channels first", update the input shape
-		# and channels dimension
-		if K.image_data_format() == "channels_first":
-			inputShape = (depth, height, width)
-			chanDim = 1
+        # if we are using "channels first", update the input shape
+        # and channels dimension
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
 
-		# setting the input and apply BN
-		inputs = Input(shape=inputShape)
-		x = BatchNormalization(axis=chanDim, epsilon=bnEps,
-			momentum=bnMom)(inputs)
+        # set the input and apply BN
+        inputs = Input(shape=inputShape)
+        x = BatchNormalization(axis=chanDim, epsilon=bnEps,
+                               momentum=bnMom)(inputs)
 
-		# applying a single CONV layer
-		x = Conv2D(filters[0], (3, 3), use_bias=False,
-			padding="same", kernel_regularizer=l2(reg))(x)
+        # check if we are utilizing the CIFAR dataset
+        if dataset == "cifar":
+            # apply a single CONV layer
+            x = Conv2D(filters[0], (3, 3), use_bias=False,
+                       padding="same", kernel_regularizer=l2(reg))(x)
 
-		# looping over the number of stages
-		for i in range(0, len(stages)):
-			# initialize the stride, then apply a residual module
-			# used to reduce the spatial size of the input volume
-			stride = (1, 1) if i == 0 else (2, 2)
-			x = ResNet.residual_module(x, filters[i + 1], stride,
-				chanDim, red=True, bnEps=bnEps, bnMom=bnMom)
+        # loop over the number of stages
+        for i in range(0, len(stages)):
+            # initialize the stride, then apply a residual module
+            # used to reduce the spatial size of the input volume
+            stride = (1, 1) if i == 0 else (2, 2)
+            x = ResNet.residual_module(x, filters[i + 1], stride,
+                                       chanDim, red=True, bnEps=bnEps, bnMom=bnMom)
 
-			# looping over the number of layers in the stage
-			for j in range(0, stages[i] - 1):
-				# apply a ResNet module
-				x = ResNet.residual_module(x, filters[i + 1],
-					(1, 1), chanDim, bnEps=bnEps, bnMom=bnMom)
+            # loop over the number of layers in the stage
+            for j in range(0, stages[i] - 1):
+                # apply a ResNet module
+                x = ResNet.residual_module(x, filters[i + 1],
+                                           (1, 1), chanDim, bnEps=bnEps, bnMom=bnMom)
 
-		# applying Batch Normalization => ACTIVATION => POOLING
-		x = BatchNormalization(axis=chanDim, epsilon=bnEps,
-			momentum=bnMom)(x)
-		x = Activation("relu")(x)
-		x = AveragePooling2D((8, 8))(x)
+        # apply BN => ACT => POOL
+        x = BatchNormalization(axis=chanDim, epsilon=bnEps,
+                               momentum=bnMom)(x)
+        x = Activation("relu")(x)
+        x = AveragePooling2D((8, 8))(x)
 
-		# softmax classifier
-		x = Flatten()(x)
-		x = Dense(classes, kernel_regularizer=l2(reg))(x)
-		x = Activation("softmax")(x)
+        # softmax classifier
+        x = Flatten()(x)
+        x = Dense(classes, kernel_regularizer=l2(reg))(x)
+        x = Activation("softmax")(x)
 
-		# creating the model
-		model = Model(inputs, x, name="resnet")
+        # create the model
+        model = Model(inputs, x, name="resnet")
 
-		# returning the constructed network architecture
-		return model
+        # return the constructed network architecture
+        return model
--- train.py
+++ train.py
@@ -1,68 +1,62 @@
-################################################################################################################################# 
-#USAGE                                                                                                                          #
-# python train_ResNet.py --train-plot output/train_no_schedule.png                                                              #															    #
-# python train_ResNet.py --schedule standard --train-plot output/train_standard_schedule.png                                    #									#
-# python train_ResNet.py --schedule step --lr-plot output/lr_step_schedule.png --train-plot output/train_step_schedule.png      #
-# python train_ResNet.py --schedule linear --lr-plot output/lr_linear_schedule.png --train-plot output/train_linear_schedule.png#
-# python train_ResNet.py --schedule poly --lr-plot output/lr_poly_schedule.png --train-plot output/train_poly_schedule.png      #
-#################################################################################################################################
-
-# setting the matplotlib backend so figures can be saved in the background
-import matplotlib
-matplotlib.use("Agg")
-
 # importing the necessary packages
-from dunkyimages.learning_rate_schedulers import StepDecay
-from dunkyimages.learning_rate_schedulers import PolynomialDecay
-from dunkyimages.resnet import ResNet
+from pyimagesearch.learning_rate_schedulers import StepDecay
+from pyimagesearch.learning_rate_schedulers import PolynomialDecay
+from pyimagesearch.resnet import ResNet
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import classification_report
-from keras.callbacks import LearningRateScheduler
-from keras.optimizers import SGD
-from keras.datasets import cifar10
+from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.datasets import cifar10
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 
+# setting the matplotlib backend so figures can be saved in the background
+import matplotlib
+
+matplotlib.use("Agg")
+
 # constructing the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--schedule", type=str, default="",
-	help="learning rate schedule method")
+                help="learning rate schedule method (standard, step, linear, poly)")
 ap.add_argument("-e", "--epochs", type=int, default=100,
-	help="# of epochs to train for")
+                help="# of epochs to train for")
 ap.add_argument("-l", "--lr-plot", type=str, default="lr.png",
-	help="path to output learning rate plot")
+                help="path to output learning rate plot")
 ap.add_argument("-t", "--train-plot", type=str, default="training.png",
-	help="path to output training plot")
+                help="path to output training plot")
 args = vars(ap.parse_args())
 
 # storing number of epochs to train for in a convenience variable,
-# then initializige the list of callbacks and learning rate scheduler to be used
+# then initialize the list of callbacks and learning rate scheduler
+# to be used
 epochs = args["epochs"]
 callbacks = []
 schedule = None
 
 # check to see if step-based learning rate decay should be used
 if args["schedule"] == "step":
-	print("[INFO] using 'step-based' learning rate decay...")
-	schedule = StepDecay(initAlpha=1e-1, factor=0.25, dropEvery=15)
+    print("[INFO] using 'step-based' learning rate decay...")
+    schedule = StepDecay(initAlpha=1e-1, factor=0.25, dropEvery=15)
 
 # checking to see if linear learning rate decay should be used
 elif args["schedule"] == "linear":
-	print("[INFO] using 'linear' learning rate decay...")
-	schedule = PolynomialDecay(maxEpochs=epochs, initAlpha=1e-1, power=1)
+    print("[INFO] using 'linear' learning rate decay...")
+    schedule = PolynomialDecay(maxEpochs=epochs, initAlpha=1e-1, power=1)
 
 # check to see if a polynomial learning rate decay should be used
 elif args["schedule"] == "poly":
-	print("[INFO] using 'polynomial' learning rate decay...")
-	schedule = PolynomialDecay(maxEpochs=epochs, initAlpha=1e-1, power=5)
+    print("[INFO] using 'polynomial' learning rate decay...")
+    schedule = PolynomialDecay(maxEpochs=epochs, initAlpha=1e-1, power=5)
 
 # if the learning rate schedule is not empty, add it to the list of
 # callbacks
 if schedule is not None:
-	callbacks = [LearningRateScheduler(schedule)]
+    callbacks = [LearningRateScheduler(schedule)]
 
-# loading the training and testing data, then scaling it into range [0, 1]
+# loading the training and testing data, then scaling it into
+# range [0, 1]
 print("[INFO] loading CIFAR-10 data...")
 ((trainX, trainY), (testX, testY)) = cifar10.load_data()
 trainX = trainX.astype("float") / 255.0
@@ -75,7 +69,7 @@ testY = lb.transform(testY)
 
 # initializing the label names for the CIFAR-10 dataset
 labelNames = ["airplane", "automobile", "bird", "cat", "deer",
-	"dog", "frog", "horse", "ship", "truck"]
+              "dog", "frog", "horse", "ship", "truck"]
 
 # initializing the decay for the optimizer
 decay = 0.0
@@ -83,29 +77,29 @@ decay = 0.0
 # When using Keras' "standard" decay, then we need to set the
 # decay parameter
 if args["schedule"] == "standard":
-	print("[INFO] using 'keras standard' learning rate decay...")
-	decay = 1e-1 / epochs
+    print("[INFO] using 'keras standard' learning rate decay...")
+    decay = 1e-1 / epochs
 
 # otherwise, no learning rate schedule is being used
 elif schedule is None:
-	print("[INFO] no learning rate schedule being used")
+    print("[INFO] no learning rate schedule being used")
 
-# initializing our optimizer and model, then compile it
+# initialize our optimizer and model, then compile it
 opt = SGD(lr=1e-1, momentum=0.9, decay=decay)
 model = ResNet.build(32, 32, 3, 10, (9, 9, 9),
-	(64, 64, 128, 256), reg=0.0005)
+                     (64, 64, 128, 256), reg=0.0005)
 model.compile(loss="categorical_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+              metrics=["accuracy"])
 
-# training the network
+# train the network
 H = model.fit(trainX, trainY, validation_data=(testX, testY),
-	batch_size=128, epochs=epochs, callbacks=callbacks, verbose=1)
+              batch_size=128, epochs=epochs, callbacks=callbacks, verbose=1)
 
-# evaluating the network
+# evaluate the network
 print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=128)
 print(classification_report(testY.argmax(axis=1),
-	predictions.argmax(axis=1), target_names=labelNames))
+                            predictions.argmax(axis=1), target_names=labelNames))
 
 # plotting the training loss and accuracy
 N = np.arange(0, args["epochs"])
@@ -121,7 +115,8 @@ plt.ylabel("Loss/Accuracy")
 plt.legend()
 plt.savefig(args["train_plot"])
 
-# if the learning rate schedule is not empty, then save the learning rate plot
+# if the learning rate schedule is not empty, then save the learning
+# rate plot
 if schedule is not None:
-	schedule.plot(N)
-	plt.savefig(args["lr_plot"])
+    schedule.plot(N)
+    plt.savefig(args["lr_plot"])
